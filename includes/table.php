6154db7b3d886d15aeeb4b079c897a432227ad0f
Mikem <Mikemangan00@gmail.com>
2016-01-05 18:00:43 -0500

    table update
    updates to the table to check for entries by asset id

--- includes/table.php
+++ includes/table.php
@@ -1,4 +1,4 @@
-<?php
+<?php $count =0;
 ?>
 
 <table>
@@ -12,9 +12,12 @@
 	</tr>
 		<?php  foreach ($assets as $asset) { // loop to add asset names?> 
 	<tr>
-		<td id="asset<?php  echo $asset['id']; ?>"><?php echo $asset['name']; ?></td>
-		<td id="treshold<?php  echo $asset['id']; ?>"><?php  echo $asset['threshold']; ?></td>
-		<td></td>
+		<td id="asset<?= $asset['id']; ?>"><?= $asset['name']; ?></td>
+		<td id="treshold<?= $asset['id']; ?>"><?= $asset['threshold']; ?></td>
+		<?php while (isset($assetsQuantites[$count]['id']) && $assetsQuantites[$count]['id'] == $asset['id']) { ?>
+		<td ><?= $assetsQuantites[$count]['quantity'] ?></td>
+		<?php $count++; } ?>
+
 	</tr>
-		<?php }?>	
+		<?php } ?>	
 </table>
